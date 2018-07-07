36bdca1947fbbe00f5ebe20283c5afa152300a51
iamnishantrai <nishant.2010rai@gmail.com>
2018-07-07 13:19:43 +0530

    Firebase Login

--- CFGApp/app/src/main/java/com/example/nishant/cfgapp/LoginActivity.java
+++ CFGApp/app/src/main/java/com/example/nishant/cfgapp/LoginActivity.java
@@ -4,10 +4,25 @@ import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgressDialog;
+    private GoogleApiClient googleApiClient;
     private ProgressDialog progressDialog;
+
+    private boolean isRegUser = false, isCabDriver = false;
+
+    private GoogleSignInClient googleSignInClient;
+    private static final int RC_SIGN_IN = 007;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
