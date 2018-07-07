5e3f0d8761505ba7efb364d3dfde1c23427dd62a
Anshul <er.anshulbly@gmail.com>
2018-07-07 15:09:53 +0530

    front page
    Admin can signup or login
    Responsive page used bootstrap

---
+++ WebApp/index.php
@@ -0,0 +1,54 @@
+
+<?php include("header.php");  ?>
+<?php include("menu.php");  ?>
+<br>
+<br>
+<br>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="css/Images/isap2.jpg" alt="New York">
+      <div class="carousel-caption">
+        <h3></h3>
+      </div> 
+    </div> 
+
+    <div class="item">
+      <img src="css/Images/isap2.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3></h3>
+      </div> 
+    </div>
+
+    <div class="item">
+      <img src="css/Images/isap2.jpg" alt="Los Angeles">
+      <div class="carousel-caption">
+        <h3></h3>
+      </div> 
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<br>
+<!--Products-->
+
+<!--<?php include("contact.php");?>-->
+</div>
+</body>
+</html>
