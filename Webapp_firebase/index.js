2e5aa8f15008949c1b7e2f19c4621d4601b8cb44
Anshul <er.anshulbly@gmail.com>
2018-07-07 22:04:16 +0530

    signup final
    All functions working now

--- Webapp_firebase/index.js
+++ Webapp_firebase/index.js
@@ -1,15 +1,45 @@
 
-var emailid=document.getElementById("emailid");
-var password= document.getElementById("password");
-var repassword=document.getElementById("repassword");
+var first_name=document.getElementById("first_name");
+var last_name= document.getElementById("last_name");
+var intials=document.getElementById("intials");
+var gender=document.getElementById("gender");
+var dob=document.getElementById("dob");
+var street= document.getElementById("street");
+var city=document.getElementById("city");
+var state=document.getElementById("state");
+var mob_no=document.getElementById("mob_no");
+var alt_no= document.getElementById("alt_no");
+var photo=document.getElementById("photo");
 var submitBtn=document.getElementById("submitBtn");
 
 function submitClick()
 {
-	var firebaseRef = firebase.database().ref();
-	var e = emailid.value;
-	var p = password.value;
-	firebaseRef.push().set(e);
-	firebaseRef.push().set(p);
+	var firebaseRef = firebase.database().ref("abcd");
+
+	var f=first_name.value;
+	var l=last_name.value;
+	var i=intials.value;
+	var g=gender.value;
+	var d=dob.value;
+	var s=street.value;
+	var c=city.value;
+	var st=state.value;
+	var mob=mob_no.value;
+	var alt=alt_no.value;
+	var ph=photo.value;
+
+	firebaseRef.child(mob).set({
+		"first_name":f,
+		"last_name":l,
+		"intials":i,
+		"gender":g,
+		"dob":d,
+		"street":s,
+		"city":c,
+		"state":st,
+		"mob_no":mob,
+		"alt_no":alt
+
+	});
 
 }
